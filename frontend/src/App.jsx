105ae1960b299e163d5d537f525c4d4834c88c7e
Sakshi Dwivedi <sakshidwivedi406@gmail.com>
2025-10-25 19:59:54 +0530

    setup of frontend done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-blue-500'>sakshi</h1>
+    </>
+  )
+}
+
+export default App
