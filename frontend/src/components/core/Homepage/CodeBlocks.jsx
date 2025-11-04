4705b6e2dd7413cceee0a538e2e92fed2957960b
Sakshi Dwivedi <sakshidwivedi406@gmail.com>
2025-11-04 09:33:43 +0530

    file updated successfully

--- frontend/src/components/core/Homepage/CodeBlocks.jsx
+++ frontend/src/components/core/Homepage/CodeBlocks.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CTAButton from "/src/components/core/HomePage/Button";
-import TypeAnimation from "react-type-animation";
+import {TypeAnimation} from "react-type-animation";
 import { FaArrowRight } from "react-icons/fa";
 
 const CodeBlocks = ({
