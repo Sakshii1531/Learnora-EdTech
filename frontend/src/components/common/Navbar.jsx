39ecbdef66795a90712cdc125e5f7bdf49334d0e
Sakshi Dwivedi <sakshidwivedi406@gmail.com>
2025-11-26 22:00:27 +0530

    catalog done

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/Logo/Logo-Full-Light.png"
 import { NavbarLinks } from "../../data/navbar-links"
@@ -6,13 +6,49 @@ import { matchPath,useLocation } from "react-router-dom"
 import { AiOutlineShoppingCart } from "react-icons/ai"
 import { useSelector } from "react-redux"
 import ProfileDropdown from "../core/Auth/ProfileDropdown"
+import { apiConnector } from "../../services/apiconnector"
+import { categories } from "../../services/apis"
+import { BsChevronDown } from "react-icons/bs"
+import { useState } from "react";
 
+
+const  subLinks = [
+    {
+        title:"python",
+        link:"/catalog/python"
+    },
+
+    {
+        title:"web-dev",
+        link:"/catalog/web-development"
+    }
+];
 
 function Navbar() {
     const { token } = useSelector((state) => state.auth)
     const { user } = useSelector((state) => state.profile)
     const { totalItems } = useSelector((state) => state.cart)
     const location = useLocation();
+
+    // const [subLinks, setSubLinks] = useState([])
+  const [loading, setLoading] = useState(false)
+
+//   const fetchSublinks = async () => {
+//     //   setLoading(true)
+//       try {
+//         const res = await apiConnector("GET", categories.CATEGORIES_API)
+//         setSubLinks(res.data.data)
+//       } catch (error) {
+//         console.log("Could not fetch Categories.", error)
+//       }
+//     //   setLoading(false)
+//     }
+    
+
+//     useEffect( () => {
+//        // fetchSublinks();
+//     },[]);
+
     const matchRoute = (route) =>{
         return matchPath({path:route} , location.pathname);
     }
@@ -30,7 +66,36 @@ function Navbar() {
                     NavbarLinks.map( (link,index) => (
                         <li key={index}>
                             {
-                                link.title === "Catalog" ? (<div></div>) : (
+                                link.title === "Catalog" ? (
+                                     <>
+                    <div
+                      className={`group relative flex cursor-pointer items-center gap-1 ${
+                        matchRoute("/catalog/:catalogName")
+                          ? "text-[#FFE83D]"
+                          : "text-[#DBDDEA]"
+                      }`}
+                    >
+                      <p>{link.title}</p>
+                      <BsChevronDown />
+                      <div className="invisible absolute left-[50%] top-[50%] z-1000 flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-[#F1F2FF] p-4 text-[#000814] opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-[#F1F2FF]"></div>
+                        { subLinks.length ? (
+                            subLinks.map((subLink, index) => (
+                                <Link
+                                  to={`${subLink.link}`}
+                                  className="rounded-lg bg-transparent py-4 pl-4 hover:bg-[#C5C7D4]"
+                                  key={index}
+                                >
+                                  <p>{subLink.title}</p>
+                                </Link>
+                              ))
+                        ) : (
+                          <p className="text-center">No Courses Found</p>
+                        )}
+                      </div>
+                    </div>
+                  </>
+                                ) : (
                                     <Link to={link?.path}>
                                         <p className={` ${matchRoute(link?.path)
                                           ? "text-[#FFE83D]"
